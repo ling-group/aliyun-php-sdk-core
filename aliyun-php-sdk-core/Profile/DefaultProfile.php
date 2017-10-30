9df49559ecd8a3e1d2f1194300d9963a9b9916a1
liujingyu <jingyu525@gmail.com>
2017-10-30 18:14:23 +0800

    fix: not found productionDomain class

--- aliyun-php-sdk-core/Profile/DefaultProfile.php
+++ aliyun-php-sdk-core/Profile/DefaultProfile.php
@@ -22,6 +22,7 @@ namespace Superman2014\Aliyun\Core\Profile;
 use Superman2014\Aliyun\Core\Auth\Credential;
 use Superman2014\Aliyun\Core\Auth\ShaHmac1Signer;
 use Superman2014\Aliyun\Core\Regions\EndpointProvider;
+use Superman2014\Aliyun\Core\Regions\ProductDomain;
 
 class DefaultProfile implements IClientProfile
 {
